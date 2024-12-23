c5bdde26f1e21f534226157b71e45c4957a3691a
rahulsamant37 <161972011+rahulsamant37@users.noreply.github.com>
2024-12-23 21:30:07 +0000

    Setup.py file ready

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import find_packages,setup
+from typing import List
+
+def get_requirements()->[str]:
+    requirement_lst:List[str]=[]
+    try:
+        with open('requirements.txt','r') as file:
+            lines=file.readlines()
+            for line in lines:
+                requirement=line.strip()
+                ## ignore empty lines and '-e .'
+                if requirement and requirement!='-e .':
+                    requirement_lst.append(requirement)
+    except FileNotFoundError:
+        print('requirements.txt file not found')
+    return requirement_lst
+
+setup(
+    name='Network-Security-System',
+    version='0.0.1',
+    author='Rahul-Samant',
+    author_email='rahulsamantcoc2@gmail.com',
+    packages=find_packages(),
+    install_requirements=get_requirements()
+)
